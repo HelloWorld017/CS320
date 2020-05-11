28c1024b50c558fe6913252567329d7d79a5eade
Khinenw <deu07115@gmail.com>
2020-05-12 02:09:32 +0900

    feat(midterm): Update midterm parser

--- src/main/scala/cs320/midterm/Midterm.scala
+++ src/main/scala/cs320/midterm/Midterm.scala
@@ -15,18 +15,10 @@ trait Midterm extends Homework with RegexParsers {
   case class Eq(l: Expr, r: Expr) extends Expr // equal-to
   case class Lt(l: Expr, r: Expr) extends Expr // less-then
   case class If(c: Expr, t: Expr, f: Expr) extends Expr // conditional
-  case class TupleE(es: List[Expr]) extends Expr // tuple
-  case class Proj(t: Expr, i: Int) extends Expr // projection
-  case object NilE extends Expr // nil
-  case class ConsE(h: Expr, t: Expr) extends Expr // cons
-  case class Empty(l: Expr) extends Expr // is-empty
-  case class Head(l: Expr) extends Expr // head
-  case class Tail(l: Expr) extends Expr // tail
   case class Val(x: String, e: Expr, b: Expr) extends Expr // local variable
   case class Fun(ps: List[String], b: Expr) extends Expr // anonymous function
   case class RecFuns(ds: List[FunDef], b: Expr) extends Expr // recursive function
   case class App(f: Expr, as: List[Expr]) extends Expr // function application
-  case class Test(e: Expr, t: Type) extends Expr // type test
 
   object Expr extends ParserObject(e)
 
@@ -50,17 +42,11 @@ trait Midterm extends Homework with RegexParsers {
     override def toString: String = this match {
       case IntV(n) => n.toString
       case BooleanV(b) => b.toString
-      case TupleV(vs) => vs.mkString("(", ", ", ")")
-      case NilV => "Nil"
-      case ConsV(h, t) => s"($h :: $t)"
       case CloV(_, _, _) => "<function>"
     }
   }
   case class IntV(n: Int) extends Value
   case class BooleanV(b: Boolean) extends Value
-  case class TupleV(vs: List[Value]) extends Value
-  case object NilV extends Value
-  case class ConsV(h: Value, t: Value) extends Value
   case class CloV(ps: List[String], b: Expr, var env: Env) extends Value
 
   // Environments
@@ -78,7 +64,7 @@ trait Midterm extends Homework with RegexParsers {
   private def wrapR[T](e: Parser[T]): Parser[T] = "(" ~> e <~ ")"
   private def wrapC[T](e: Parser[T]): Parser[T] = "{" ~> e <~ "}"
   private def wrapS[T](e: Parser[T]): Parser[T] = "[" ~> e <~ "]"
-  private lazy val keywords = Set("true", "false", "val", "def", "Nil", "if", "else")
+  private lazy val keywords = Set("true", "false", "val", "def", "if", "else")
   private lazy val n: Parser[Int] = "-?[0-9]+".r ^^ (_.toInt)
   private lazy val i: Parser[Int] = "_[1-9][0-9]*".r ^^ (_.tail.toInt)
   private lazy val b: Parser[Boolean] =
@@ -118,66 +104,47 @@ trait Midterm extends Homework with RegexParsers {
       case (l, "/" ~ r) => Div(l, r)
       case (l,  _  ~ r) => Mod(l, r)
     }}
+
   private lazy val e6: Parser[Expr] =
     "-" ~> e6 ^^ Neg | "!" ~> e6 ^^ Not | e7
+
   private lazy val e7: Parser[Expr] =
     e8 ~ rep(
       wrapR(repsep(e, ",")) ^^ AppP |
-      "." ~> i ^^ ProjP |
-      "." ~> "isEmpty" ^^^ EmptyP |
-      "." ~> "nonEmpty" ^^^ NonEmptyP |
-      "." ~> "head" ^^^ HeadP |
-      "." ~> "tail" ^^^ TailP |
-      "." ~> "isInstanceOf" ~> wrapS(t) ^^ TestP
+      "." ~> x ^^ ObjGetP |
+      ("." ~> x <~ "=") ~ e ~ (";" ~> e) ^^ ObjSetP
     ) ^^ { case e ~ es => es.foldLeft(e){
       case (f, AppP(as)) => App(f, as)
-      case (t, ProjP(i)) => Proj(t, i)
-      case (l, EmptyP) => Empty(l)
-      case (l, NonEmptyP) => NonEmpty(l)
-      case (l, HeadP) => Head(l)
-      case (l, TailP) => Tail(l)
-      case (e, TestP(t)) => Test(e, t)
+      case (obj, ObjGetP(name)) => ObjGet(obj, name)
+      case (obj, ObjSetP(name, value, expr)) => ObjSet(obj, name, value, expr)
     }}
+
   private lazy val e8: Parser[Expr] =
-    x ^^ Id | n ^^ IntE | b ^^ BooleanE | "Nil" ^^^ NilE |
-    wrapR(rep1sep(e, ",")) ^^ {
-      case e :: Nil => e
-      case es => TupleE(es)
-    } |
+    x ^^ Id | n ^^ IntE | b ^^ BooleanE | "{}" ^^^ ObjE |
     ("if" ~> wrapR(e)) ~ e ~ ("else" ~> e) ^^ { case c ~ t ~ f => If(c, t, f) } |
     ("val" ~> x <~ "=") ~ e ~ (";" ~> e) ^^ { case x ~ e ~ b => Val(x, e, b) } |
     (("val" ~> wrapR((x <~ ",") ~ rep1sep(x, ",")) <~ "=") ~ e ~ (";" ~> e)).withFilter{
       case x ~ xs ~ e ~ b =>
         val xs1 = x :: xs
         xs1.distinct.length == xs1.length
-    } ^^ {
-      case x ~ xs ~ e ~ b => TupleVal(x :: xs, e, b)
     } |
     (rep1(d) ~ e).withFilter{ case ds ~ _ =>
       val names = ds.map(_.n)
       names.distinct.length == names.length
     } ^^ { case ds ~ b => RecFuns(ds, b) } |
-    wrapC(e) | e9
+    wrapC(e)
+
   private lazy val d: Parser[FunDef] =
     (("def" ~> x) ~ wrapR(repsep(x, ",")) ~ ("=" ~> e <~ ";")).withFilter{
       case _ ~ ps ~ _ => ps.distinct.length == ps.length
     } ^^ {
       case n ~ ps ~ b => FunDef(n, ps, b)
     }
-  private lazy val t: Parser[Type] =
-    "Int" ^^^ IntT |
-    "Boolean" ^^^ BooleanT |
-    "Tuple" ^^^ TupleT |
-    "List" ^^^ ListT |
-    "Function" ^^^ FunctionT
-  private sealed trait E6P
-  private case class AppP(as: List[Expr]) extends E6P
-  private case class ProjP(i: Int) extends E6P
-  private case object EmptyP extends E6P
-  private case object NonEmptyP extends E6P
-  private case object HeadP extends E6P
-  private case object TailP extends E6P
-  private case class TestP(t: Type) extends E6P
+
+  private sealed trait E3P
+  private case class AppP(as: List[Expr]) extends E3P
+  private case class ObjGetP(name: string) extends E3P
+  private case class ObjSetP(name: string, value: Expr, expr: Expr) extends E6P
 
   // Desugaring
   private val T = BooleanE(true)
@@ -196,14 +163,6 @@ trait Midterm extends Homework with RegexParsers {
   private def Gte(l: Expr, r: Expr): Expr = Not(Lt(l, r))
   private def And(l: Expr, r: Expr): Expr = If(l, r, F)
   private def Or(l: Expr, r: Expr): Expr = If(l, T, r)
-  private def NonEmpty(l: Expr): Expr = Not(Empty(l))
-  private def TupleVal(xs: List[String], e: Expr, b: Expr): Expr = {
-    require(xs.length > 1)
-    val t = fresh()
-    Val(t, e, xs.zipWithIndex.foldRight(b){
-      case ((x, i), b) => Val(x, Proj(Id(t), i + 1), b)
-    })
-  }
   private var id = -1
   private def fresh(): String = {
     id += 1
